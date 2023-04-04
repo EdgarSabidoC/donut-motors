c9ab46fc568a452902d6eaa3371e8fcba8320d75
Edgar Sabido C <edgar.sabido17@gmail.com>
2023-04-04 12:10:40 -0600

    Se agregó: Plantilla para el módulo del router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: AppComponent },
+  // { path: 'busqueda/pokedex', redirectTo: 'busqueda/pokedex/pikachu', pathMatch: 'full', component: PokedexComponent },
+  // { path: 'busqueda/pokedex/:query', component: PokedexComponent,
+  //   data: { title: 'Pokedex' }
+  // },
+  // { path: 'busqueda/digivice', redirectTo: 'busqueda/digivice/agumon', pathMatch: 'full', component: DigiviceComponent },
+  // { path: 'busqueda/digivice/:query', component: DigiviceComponent,
+  //   data: { title: 'Digivice' }
+  // },
+  // { path: 'busqueda/tvshows', redirectTo: 'busqueda/tvshows/pokemon', pathMatch: 'full', component: TvshowsComponent },
+  // { path: 'busqueda/tvshows/:query', component: TvshowsComponent,
+  //   data: { title: 'TV Shows' }
+  // },
+  // { path: '**', component: Error404 } //error 404
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
