7d3cd97cc4328d9d7270f8aca73b74dd81016548
Edgar Sabido C <edgar.sabido17@gmail.com>
2023-04-07 19:03:34 -0600

    Se modificaron los componentes de registro de color, dealership, maker, model y car

--- src/app/components/register-a-maker/register-a-maker.component.ts
+++ src/app/components/register-a-maker/register-a-maker.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register-a-maker',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-a-maker.component.scss']
 })
 export class RegisterAMakerComponent {
+  register_a_maker_form!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
+  // Formulario de registro del fabricante de autos:
+  ngOnInit(): void {
+    this.register_a_maker_form = this.fb.group({
+      name: this.fb.control(null, [Validators.required]),
+      logo_image: this.fb.control(null, [Validators.required, this.imageValidator]),
+    });
+  }
+
+  // Valida que la imagen seleccionada para la foto del auto sea de tipo jpeg, webp, png o avif:
+  imageValidator(control: AbstractControl): { [key: string]: any } | null {
+    const allowedFormats = ['jpg','jpeg', 'webp', 'png', 'avif'];
+    const file = control.value;
+    if (file && file.name) { // Se comprueba si existe el archivo y el nombre.
+      const fileExt = file.name.split('.').pop()?.toLowerCase();
+      if (allowedFormats.indexOf(fileExt) === -1) {
+        return { invalidImageFormat: true };
+      }
+    }
+    return null;
+  }
+
+  // Guarda el formulario.
+  onSaveForm() {
+    if (this.register_a_maker_form.valid) {
+      console.log("Saved!");
+      console.log("El nombre del fabricante es: ", this.register_a_maker_form.get('name')?.value);
+    }
+  }
 }
--- src/app/components/register-a-model/register-a-model.component.ts
+++ src/app/components/register-a-model/register-a-model.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register-a-model',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-a-model.component.scss']
 })
 export class RegisterAModelComponent {
+  register_a_model_form!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
+  // Formulario de registro del modelo de un auto:
+  ngOnInit(): void {
+    this.register_a_model_form = this.fb.group({
+      name: this.fb.control(null, [Validators.required]),
+    });
+  }
+
+  // Guarda el formulario.
+  onSaveForm() {
+    if (this.register_a_model_form.valid) {
+      console.log("Saved!");
+      console.log("El nombre del modelo es: ", this.register_a_model_form.get('name')?.value);
+    }
+  }
 }
